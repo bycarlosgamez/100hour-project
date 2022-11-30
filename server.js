ded30c613817cb4387ecaf1cbe9266177c2d68ec
Carlos Gámez <hardpx@gmail.com>
2022-11-29 20:46:49 -0600

    added a basi form and post request to create new tickest

--- server.js
+++ server.js
@@ -14,15 +14,33 @@ connectDB();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public')); // to use public files
+app.use(express.urlencoded({ extended: true })); // to parse req.body
+app.use(express.json()); // to convert to json
+
+// homepage
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+// see all tickets
 app.get('/tickets', async (req, res) => {
   const tickets = await Ticket.find({});
   res.render('tickets/index', { tickets });
 });
 
+// create new ticket
+app.get('/tickets/new', (req, res) => {
+  res.render('tickets/new');
+});
+
+app.post('/tickets', async (req, res) => {
+  const ticket = new Ticket(req.body.ticket);
+  await ticket.save();
+  res.redirect(`/tickets/${ticket._id}`);
+});
+
+// see individual ticket by id
 app.get('/tickets/:id', async (req, res) => {
   const ticket = await Ticket.findById(req.params.id);
   res.render('tickets/show', { ticket });
