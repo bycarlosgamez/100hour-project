94339cbcf85e82be0e13a8654f2051af613f5eda
Carlos Gámez <hardpx@gmail.com>
2022-12-08 16:28:11 -0600

    updated ticket model and change options on views to reflect model changes

--- models/ticket.js
+++ models/ticket.js
@@ -2,15 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TicketSchema = new Schema({
-  title: String,
-  description: String,
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   owner: String,
   assigned: String,
-  created: Date,
-  updated: Date,
-  type: String,
-  priority: String,
-  status: String,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+  type: {
+    type: String,
+    defaul: 'bug',
+    enum: ['bug', 'feature', 'document', 'other'],
+  },
+  priority: {
+    type: String,
+    defaul: 'high',
+    enum: ['high', 'medium', 'low'],
+  },
+  status: {
+    type: String,
+    defaul: 'open',
+    enum: ['open', 'solved', 'awaiting'],
+  },
   project: String,
 });
 
