c491f2f677e53bd915cf7cebc08fa0a0c7ea7368
Carlos Gámez <hardpx@gmail.com>
2022-12-12 21:42:36 -0600

    added express-session and basic session and cookie configurations

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejsmate = require('ejs-mate');
-const catchAsync = require('./helpers/catchAsync');
 const ExpressError = require('./helpers/ExpressError');
 const methodOverride = require('method-override');
+const session = require('express-session');
 const connectDB = require('./config/database');
-const Ticket = require('./models/ticket');
-const Comment = require('./models/comment');
 
 // Routers
 const tickets = require('./routes/tickets');
 const comments = require('./routes/comments');
+
+// Session Config
+const sessionConfig = {
+  secret: 'secretstringchangeinproduction',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
 // Allows us to hace acces to varialves in our .env file
 require('dotenv').config({ path: './config/.env' });
@@ -23,10 +33,12 @@ app.engine('ejs', ejsmate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// configurations
 app.use(express.static('public')); // to use public files
 app.use(express.urlencoded({ extended: true })); // to parse req.body
 app.use(express.json()); // to convert to json
 app.use(methodOverride('_method')); // to use put or delete methods from form
+app.use(session(sessionConfig)); // Session (express-session middleware)
 
 // Routes
 app.use('/tickets', tickets);
