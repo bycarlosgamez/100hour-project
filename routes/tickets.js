f69ea82f4a14569407c11af301b4a5e624b6bb66
Carlos Gámez <hardpx@gmail.com>
2022-12-12 18:07:24 -0600

    added a routes folder and create a router for tickets

---
+++ routes/tickets.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../helpers/catchAsync');
+const ExpressError = require('../helpers/ExpressError');
+const Ticket = require('../models/ticket');
+
+// see all tickets
+router.get(
+  '/',
+  catchAsync(async (req, res, next) => {
+    const tickets = await Ticket.find({});
+    res.render('tickets/index', { tickets });
+  })
+);
+
+// create new ticket
+router.get('/new', (req, res) => {
+  res.render('tickets/new');
+});
+
+router.post(
+  '/',
+  catchAsync(async (req, res) => {
+    if (!req.body.ticket) throw new ExpressError('Ivalid Ticket Data', 400);
+    const ticket = new Ticket(req.body.ticket);
+    await ticket.save();
+    res.redirect(`/tickets/${ticket._id}`);
+  })
+);
+
+// see individual ticket by id
+router.get(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const ticket = await Ticket.findById(id).populate('comments');
+    res.render('tickets/show', { ticket });
+  })
+);
+
+// edit existing ticket
+router.get(
+  '/:id/edit',
+  catchAsync(async (req, res) => {
+    const ticket = await Ticket.findById(req.params.id);
+    res.render('tickets/edit', { ticket });
+  })
+);
+
+router.put(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const ticket = await Ticket.findByIdAndUpdate(id, { ...req.body.ticket });
+    res.redirect(`/tickets/${ticket._id}`);
+  })
+);
+
+// delete ticket
+router.delete(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Ticket.findByIdAndDelete(id);
+    res.redirect(`/tickets`);
+  })
+);
+
+module.exports = router;
