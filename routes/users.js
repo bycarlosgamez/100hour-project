cd8cf62161f731a60fd8dfaeb658ed2aa4012da7
Carlos Gámez <hardpx@gmail.com>
2022-12-15 17:57:04 -0600

    finish post route to add signup a user and show flash message if user already in db

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../helpers/catchAsync');
-const { register } = require('../models/user');
 const User = require('../models/user');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
+router.get('/signup', (req, res) => {
+  res.render('users/signup');
 });
 
 router.post(
-  '/register',
+  '/signup',
   catchAsync(async (req, res) => {
-    res.send(req.body);
-    // const user = new User(req.body);
-    // await user.save();
-    // req.flash('success', 'Successfully created an account');
-    // res.redirect(`/tickets/${ticket._id}`);
+    try {
+      const { username, email, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash('success', 'Welcome to BugTracker');
+      res.redirect(`/tickets`); //change to redirect to dashboard
+    } catch (err) {
+      req.flash('error', err.message);
+      res.redirect('/signup');
+    }
   })
 );
-
-router.post('/');
 
 module.exports = router;
