ed41564c1ba0eec25ba3b3fa31639574767b9713
Carlos Gámez <hardpx@gmail.com>
2022-12-16 10:22:02 -0600

    added post route to auteticate localally with passport

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const catchAsync = require('../helpers/catchAsync');
 const User = require('../models/user');
 
+// @description     Local Signup
+// @route GET       users/signup
 router.get('/signup', (req, res) => {
   res.render('users/signup');
 });
@@ -19,6 +22,29 @@ router.post(
     } catch (err) {
       req.flash('error', err.message);
       res.redirect('/signup');
+    }
+  })
+);
+
+// @description     Local Login
+// @route GET       users/login
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login',
+  }),
+  catchAsync(async (req, res) => {
+    try {
+      req.flash('success', 'Welcome back');
+      res.redirect('/tickets');
+    } catch (err) {
+      req.flash('error', err.message);
+      res.redirect('/login');
     }
   })
 );
