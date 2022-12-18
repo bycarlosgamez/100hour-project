e8ebef32adbf151001019db78dde3a03e22630f0
Carlos Gámez <hardpx@gmail.com>
2022-12-18 16:05:03 -0600

    added current user to local and changed name from server to app

--- app.js
+++ app.js
@@ -52,8 +52,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash middleware
+// Locals
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
