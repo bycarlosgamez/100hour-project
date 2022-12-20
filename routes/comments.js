bfa9375a0a74fc064609e0539d0647ff31184393
Carlos Gámez <hardpx@gmail.com>
2022-12-19 21:51:06 -0600

    refactored to use comment controllers

--- routes/comments.js
+++ routes/comments.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../helpers/catchAsync');
-const Ticket = require('../models/ticket');
-const Comment = require('../models/comment');
-const { isLoggedIn, isOwner, isCommentAuthor } = require('../middleware/auth');
+const { isLoggedIn, isCommentAuthor } = require('../middleware/auth');
+const commentsController = require('../controllers/comments');
 
 // @description     Process Add comment form
 // @route           POST tickets/
-router.post(
-  '/',
-  isLoggedIn,
-  catchAsync(async (req, res) => {
-    const ticket = await Ticket.findById(req.params.id);
-    const comment = new Comment(req.body.comment);
-    comment.author = req.user._id;
-    ticket.comments.push(comment);
-    await comment.save();
-    await ticket.save();
-    req.flash('success', 'Comment posted');
-    res.redirect(`/tickets/${ticket._id}`);
-  })
-);
+router.post('/', isLoggedIn, catchAsync(commentsController.createComment));
 
 // @description     Process Delete comment form
 // @route           Delete tickets/commentId
@@ -28,13 +14,7 @@ router.delete(
   '/:commentId',
   isLoggedIn,
   isCommentAuthor,
-  catchAsync(async (req, res) => {
-    const { id, commentId } = req.params;
-    await Ticket.findByIdAndUpdate(id, { $pull: { comments: commentId } });
-    await Comment.findByIdAndDelete(commentId);
-    req.flash('success', 'Comment deleted');
-    res.redirect(`/tickets/${id}`);
-  })
+  catchAsync(commentsController.deleteComment)
 );
 
 module.exports = router;
