671ab7e4b6f1ffe929f2ad8eedd1bfaf46a0a1a7
Carlos Gámez <hardpx@gmail.com>
2022-12-19 19:22:37 -0600

    refactored to use ticket controllers

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,62 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../helpers/catchAsync');
-const ExpressError = require('../helpers/ExpressError');
-const Ticket = require('../models/ticket');
 const { isLoggedIn, isOwner } = require('../middleware/auth');
+const ticketsController = require('../controllers/tickets');
 
 // @description     Show all available tickets
 // @route           GET /tickets
-router.get(
-  '/',
-  isLoggedIn,
-  catchAsync(async (req, res, next) => {
-    const tickets = await Ticket.find({}).populate('owner');
-    res.render('tickets/index', { tickets });
-  })
-);
+router.get('/', isLoggedIn, catchAsync(ticketsController.getTickets));
 
 // @description     Show new ticket page
 // @route           GET /tickets/new
-router.get('/new', isLoggedIn, (req, res) => {
-  res.render('tickets/new');
-});
+router.get('/new', isLoggedIn, ticketsController.createTicketForm);
 
 // @description     Process Create ticket form
 // @route           POST /tickets
-router.post(
-  '/',
-  isLoggedIn,
-  catchAsync(async (req, res) => {
-    const ticket = new Ticket(req.body.ticket);
-    ticket.owner = req.user._id;
-    await ticket.save();
-    req.flash('success', 'Successfully created a new ticket');
-    res.redirect(`/tickets/${ticket._id}`);
-  })
-);
+router.post('/', isLoggedIn, catchAsync(ticketsController.createTicket));
 
 // @description     Show individual ticket page by id
 // @route           GET /tickets/:id
-router.get(
-  '/:id',
-  isLoggedIn,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const ticket = await Ticket.findById(id)
-      .populate({
-        path: 'comments',
-        populate: { path: 'author' },
-      })
-      .populate('owner');
-    console.log(ticket);
-    if (!ticket) {
-      req.flash('error', 'Cannot find ticket');
-      res.redirect('/tickets');
-    }
-    res.render('tickets/show', { ticket });
-  })
-);
+router.get('/:id', isLoggedIn, catchAsync(ticketsController.showTicket));
 
 // @description     Show edit ticket page
 // @route           GET /tickets/:id/edit
@@ -64,16 +26,7 @@ router.get(
   '/:id/edit',
   isLoggedIn,
   isOwner,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const ticket = await Ticket.findById(id);
-    if (!ticket) {
-      req.flash('error', 'Cannot find ticket');
-      res.redirect('/tickets');
-    }
-
-    res.render('tickets/edit', { ticket });
-  })
+  catchAsync(ticketsController.editTicketForm)
 );
 
 // @description     Process Edit ticket form
@@ -82,11 +35,7 @@ router.put(
   '/:id',
   isLoggedIn,
   isOwner,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const ticket = await Ticket.findByIdAndUpdate(id, { ...req.body.ticket });
-    res.redirect(`/tickets/${ticket._id}`);
-  })
+  catchAsync(ticketsController.editTicket)
 );
 
 // @description     Process Delete ticket form
@@ -95,12 +44,7 @@ router.delete(
   '/:id',
   isLoggedIn,
   isOwner,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Ticket.findByIdAndDelete(id);
-    req.flash('success', 'Ticket deleted');
-    res.redirect(`/tickets`);
-  })
+  catchAsync(ticketsController.deleteTicket)
 );
 
 module.exports = router;
