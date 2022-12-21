82c5743e258bb9006996b2c3c6c1634942b6f488
Carlos Gámez <hardpx@gmail.com>
2022-12-21 13:42:31 -0600

    added is owner to auth middleware to check owner of ticket

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,21 @@
-module.exports.isLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
-    req.flash('error', 'You must be logged in');
-    return res.redirect('/login');
-  }
-  next();
+const Ticket = require('../models/ticket');
+
+module.exports = {
+  isLoggedIn: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      req.session.returnTo = req.originalUrl;
+      req.flash('error', 'You must be logged in');
+      return res.redirect('/login');
+    }
+    next();
+  },
+  isOwner: async function (req, res, next) {
+    const { id } = req.params;
+    const ticket = await Ticket.findById(id);
+    if (!ticket.owner.equals(req.user._id)) {
+      req.flash('error', 'You do not have permission to perform this action');
+      res.redirect(`/tickets/${id}`);
+    }
+    next();
+  },
 };
