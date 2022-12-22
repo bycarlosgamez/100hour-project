9602ea8d8cc09fba9e473bde65785eb1d305c682
Carlos Gámez <hardpx@gmail.com>
2022-12-22 10:50:46 -0600

    added commnt authr to posr route and auth middleware to post and delete routes

--- routes/comments.js
+++ routes/comments.js
@@ -3,13 +3,17 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../helpers/catchAsync');
 const Ticket = require('../models/ticket');
 const Comment = require('../models/comment');
+const { isLoggedIn, isOwner, isCommentAuthor } = require('../middleware/auth');
 
-// add ticket comments
+// @description     Process Add comment form
+// @route           POST tickets/
 router.post(
   '/',
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const ticket = await Ticket.findById(req.params.id);
     const comment = new Comment(req.body.comment);
+    comment.author = req.user._id;
     ticket.comments.push(comment);
     await comment.save();
     await ticket.save();
@@ -18,9 +22,12 @@ router.post(
   })
 );
 
-// delete ticket comment
+// @description     Process Delete comment form
+// @route           Delete tickets/commentId
 router.delete(
   '/:commentId',
+  isLoggedIn,
+  isCommentAuthor,
   catchAsync(async (req, res) => {
     const { id, commentId } = req.params;
     await Ticket.findByIdAndUpdate(id, { $pull: { comments: commentId } });
